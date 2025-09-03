0a893d8dcafa203070aba91ab188c8bc7cecfd93
chaolingting <10724352+chaolingting@users.noreply.github.com>
2025-09-03 16:05:40 +0800

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let number = 0; //總和
+
+function operateAdd(a, b){
+    let number = a + b;
+
+}
+
+function operateSubtract(a, b){
+    let number = a - b;
+}
+
+function operateMultiply(a, b){
+    let number = a * b;
+}
+
+function operateDivide(a, b){
+    let number =  a / b;
+}
+
+

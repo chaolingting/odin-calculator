fb1781a345761fc3c2b704667e906985fd653cb3
chaolingting <10724352+chaolingting@users.noreply.github.com>
2025-09-04 09:12:18 +0800

    added basic elements to html javascript

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-let number = 0; //總和
+let firstNum = 0; //第一個數字
+let secondNum = 0; //第二個數字
+let symbol = ""; //加減乘除
 
-function operateAdd(a, b){
-    let number = a + b;
-
+function operate(firstNum, secondNum, symbol){
+    if (symbol === "+"){
+        return firstNum + secondNum;
+    }
+    if (symbol === "-"){
+        return firstNum - secondNum;
+    }
+    if (symbol === "*"){
+        return firstNum * secondNum;
+    }
+    if (symbol === "/"){
+        return firstNum / secondNum;
+    }
 }
 
-function operateSubtract(a, b){
-    let number = a - b;
-}
+const screen =  document.getElementById("screen")
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+const four = document.getElementById("four")
+const five = document.getElementById("five")
+const six = document.getElementById("six")
+const seven = document.getElementById("seven")
+const eight = document.getElementById("eight")
+const nine = document.getElementById("nine")
+const zero = document.getElementById("zero")
+//const add = document.getElementById("add")
+//const multiply = document.getElementById("multiply")
+//const divide = document.getElementById("divide")
+//const equal = document.getElementById("equal")
+//const clear = document.getElementById("clear")
 
-function operateMultiply(a, b){
-    let number = a * b;
-}
 
-function operateDivide(a, b){
-    let number =  a / b;
-}
 
+
+one.addEventListener("click", function(){
+    screen.textContent += "1"
+})
+
+two.addEventListener("click", () => screen.textContent += "2")
+three.addEventListener("click", () => screen.textContent += "3")
 

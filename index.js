79120ff5fc60b5475d67c56619c7ea3e0043f133
chaolingting <10724352+chaolingting@users.noreply.github.com>
2025-09-16 17:32:35 +0800

    added objects

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const addBtn = document.querySelector(".add")
-const subtractBtn = document.querySelector(".subtract")
-const multiplyBtn = document.querySelector(".multiply")
-const divideBtn = document.querySelector(".divide")
+const addBtn = document.querySelector(".add");
+const subtractBtn = document.querySelector(".subtract");
+const multiplyBtn = document.querySelector(".multiply");
+const divideBtn = document.querySelector(".divide");
+const numBtns = document.querySelectorAll(".num");
+
+const numbers = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9 
+}
+
+
+numBtns.forEach(btn =>{
+    btn.addEventListener("click", ()=>{
+        const num = btn.getAttribute("data-value")
+        console.log(numbers[num])
+    })
+} )
+
 
 let add = false;
 let subtract = false;
@@ -14,8 +37,10 @@ multiplyBtn.addEventListener("click", () => {multiply = true; console.log(operat
 divideBtn.addEventListener("click", () => {divide = true; console.log(operator(firstValue, secondValue)); divide = false})
 
 
-let firstValue = 90
-let secondValue = 50
+let firstValue = function(){
+    return numbers.value
+
+}
 
 
 const operator = function operator(firstValue, secondValue){

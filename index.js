05aca28da32a9897a7d37cde600466acd14c2ade
chaolingting <10724352+chaolingting@users.noreply.github.com>
2025-09-16 16:48:49 +0800

    added operator function

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
-let firstNum = 0; //第一個數字
-let secondNum = 0; //第二個數字
-let symbol = ""; //加減乘除
+const addBtn = document.querySelector(".add")
+const subtractBtn = document.querySelector(".subtract")
+const multiplyBtn = document.querySelector(".multiply")
+const divideBtn = document.querySelector(".divide")
 
-function operate(firstNum, secondNum, symbol){
-    if (symbol === "+"){
-        return firstNum + secondNum;
-    }
-    if (symbol === "-"){
-        return firstNum - secondNum;
-    }
-    if (symbol === "*"){
-        return firstNum * secondNum;
-    }
-    if (symbol === "/"){
-        return firstNum / secondNum;
+let add = false;
+let subtract = false;
+let multiply = false;
+let divide = false;
+
+addBtn.addEventListener("click", () => {add = true; console.log(operator(firstValue, secondValue)); add = false})
+subtractBtn.addEventListener("click", () => {subtract = true; console.log(operator(firstValue, secondValue)); subtract = false})
+multiplyBtn.addEventListener("click", () => {multiply = true; console.log(operator(firstValue, secondValue)); multiply = false})
+divideBtn.addEventListener("click", () => {divide = true; console.log(operator(firstValue, secondValue)); divide = false})
+
+
+let firstValue = 90
+let secondValue = 50
+
+
+const operator = function operator(firstValue, secondValue){
+    if (add){
+        return firstValue += secondValue;
+    } else if (subtract){
+        return firstValue -= secondValue;
+    } else if (multiply){
+        return firstValue *= secondValue;
+    } else if (divide){
+        return firstValue /= secondValue;   
     }
 }
 
-const screen =  document.getElementById("screen")
-const one = document.getElementById("one")
-const two = document.getElementById("two")
-const three = document.getElementById("three")
-const four = document.getElementById("four")
-const five = document.getElementById("five")
-const six = document.getElementById("six")
-const seven = document.getElementById("seven")
-const eight = document.getElementById("eight")
-const nine = document.getElementById("nine")
-const zero = document.getElementById("zero")
-//const add = document.getElementById("add")
-//const multiply = document.getElementById("multiply")
-//const divide = document.getElementById("divide")
-//const equal = document.getElementById("equal")
-//const clear = document.getElementById("clear")
 
 
-
-
-one.addEventListener("click", function(){
-    screen.textContent += "1"
-})
-
-two.addEventListener("click", () => screen.textContent += "2")
-three.addEventListener("click", () => screen.textContent += "3")
 
---
+++ readme.md
@@ -0,0 +1,16 @@
+pseudocode
+
+add(+)
+substract(-)
+multiply(*)
+divide(/)
+clear(ac)
+equal(=)
+
+first number + operator + second number
+--> (variable) (variable/function) (variable)
+
+a display screen 
+--> (variable)
+
+can store the content of the display in a variable for use in the next step
